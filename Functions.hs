7ad23065df544c4abca7db0966e44d35075d97c4
lord_santanna <adriacabeza@hotmail.com>
2018-09-20 16:43:09 +0200

    Add files via upload

--- Functions.hs
+++ Functions.hs
@@ -1,14 +1,16 @@
 eql :: [Int] -> [Int] -> Bool 
-eql listA listB =  listA == listB
+-- eql listA listB =  listA == listB
+eql a b = (length(a) == length(b))  && (and (zipWith (==) a b))
 
 prod :: [Int] -> Int 
-prod x = (product x)
+-- prod x = (product x)
+prod x = foldl (*) 1 x 
 
--- prodOfEvens :: [Int] -> Int 
+prodOfEvens :: [Int] -> Int 
+prodOfEvens x = foldl (*) 1 (filter even x)
 
--- -- fer servir la eveno per saber si es un numero parell
-
--- powersOf2 :: [Int]
+powersOf2 :: [Int]
+powersOf2 = iterate (*2) 1
     
 scalarProduct :: [Float] -> [Float] -> Float
-scalarProduct x y  = sum $ zipWith (*) x y
+scalarProduct x y = sum $ zipWith (*) x y
