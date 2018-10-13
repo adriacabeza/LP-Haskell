8450b9e74cf3fd8676216198929b02b7870664f1
lord_santanna <adriacabeza@hotmail.com>
2018-10-13 21:37:13 +0200

    Added factorials and tartaglia

--- infinity.hs
+++ infinity.hs
@@ -8,6 +8,11 @@ fib = 0:1:zipWith (+) fib (tail fib)
 nats:: [Integer]
 nats = iterate (+1) 0
 
+
+triangulars :: [Integer]
+triangulars = 0:(scanl (+) 1 $ iterate (+1) 2)
+
+
 ints :: [Integer]
 ints = iterate (integer) 0
      where
@@ -16,6 +21,24 @@ ints = iterate (integer) 0
                | x > 0 = (-x)
                | otherwise = (-x) + 1
 
+
+hasDivisors :: Integer -> Integer -> Bool
+hasDivisors n c 
+    | c * c > n     = False
+    | mod n c == 0  = True
+    | otherwise = hasDivisors n (c + 1)
+    
+
+isPrime :: Integer -> Bool
+isPrime 0 = False
+isPrime 1 = False
+isPrime n = not (hasDivisors n 2)
+
+primes:: [Integer]
+primes = filter (isPrime) (iterate (+1) 2) 
+
+factorials :: [Integer]
+factorials = scanl (*) 1 $ iterate (+1) 1 
 
 hammings:: [Integer]
 hammings =  1: merge (map(*2) hammings) (map(*3) hammings) (map(*5) hammings)
@@ -32,27 +55,23 @@ merge' (x:xs) (y:ys)
     |otherwise  = merge' (x:xs) ys
 
 
+tartaglia :: [[Integer]]
+tartaglia = iterate next [1]
 
-data Queue a = Queue [a] [a]
-     deriving (Show)
- 
-create :: Queue a
-create = Queue [] []
- 
-push :: a -> Queue a -> Queue a
-push x (Queue b c) = Queue b (x:c)
+next::[Integer]->[Integer]
+next x = zipWith (+) ([0] ++ x) (x ++ [0])
 
-pop :: Queue a -> Queue a
-pop (Queue [] b) = pop Queue (reverse(b)++[]) []
-pop (Queue (x:xs) c) = Queue xs c 
+--aquest l he copiat: 
+lookNsay :: [Integer]
+lookNsay = iterate count 1
 
-top :: Queue a -> a
--- top (Queue [] x) = last x
-top (Queue (x:xs) _) = x  
-  
-empty :: Queue a -> Bool
-empty (Queue [] []) = True
-empty (Queue a b) = False 
- 
---  arbres1,2,3,4
+count :: Integer -> Integer
+count a = read $ next' $ show a
 
+next' :: [Char] -> [Char]
+next' [] = []
+next' cs = (show n) ++ [pr] ++ next' cua
+  where 
+    pr = head cs
+    n = length $ takeWhile ( == pr) cs
+    cua = dropWhile ( == pr) cs
